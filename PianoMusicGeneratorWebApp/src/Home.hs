470a716fa1df92e8e6b40acbb76285fb761c03a4
Joe Barela <joebarela18@gmail.com>
2023-10-17 09:46:05 -0600

    Basic Hello World Web App template

---
+++ PianoMusicGeneratorWebApp/src/Home.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+module Home where
+
+import Foundation
+import Yesod.Core
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|Hello, World!|]
