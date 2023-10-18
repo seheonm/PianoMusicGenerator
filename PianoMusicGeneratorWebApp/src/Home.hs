5a371ad812577264927616388a532fc1a7e93892
Joe Barela <joebarela18@gmail.com>
2023-10-17 21:21:57 -0600

    Added sound functionality and button

--- PianoMusicGeneratorWebApp/src/Home.hs
+++ PianoMusicGeneratorWebApp/src/Home.hs
@@ -8,5 +8,13 @@ import Yesod.Core
 getHomeR :: Handler Html
 getHomeR = defaultLayout [whamlet|
 Hello, World!
+<audio id="myAudio">
+    <source src=@{StaticR _Recording_m4a} type="audio/x-m4a">
+<button onclick="playAudio()">Play Sound</button>
+<script>
+    function playAudio() {
+        var x = document.getElementById("myAudio");
+        x.play();
+    }
 <img src=@{StaticR _IMG_1960_jpg}>
 |]
