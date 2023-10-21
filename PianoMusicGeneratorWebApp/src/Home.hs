d2e76aefab8499be9914e753f2b1ebbf4ca62f43
marinaseheon <seheonmarina@gmail.com>
2023-10-21 12:54:31 -0600

    piano

--- PianoMusicGeneratorWebApp/src/Home.hs
+++ PianoMusicGeneratorWebApp/src/Home.hs
@@ -12,23 +12,50 @@ playNote p = play $ note qn p
 getPlayNoteR :: Pitch -> Handler Html
 getPlayNoteR p = liftIO (playNote p) >> return [shamlet|Note played|]
 
-getHomeR :: Handler Html
-getHomeR = defaultLayout [whamlet|
-    Hello, welcome to our Piano Music Generator!
-    <button onclick="playNote('C4')">C</button>
-    <button onclick="playNote('Cs4')">C#</button>
-    <button onclick="playNote('D4')">D</button>
-    <button onclick="playNote('Ds4')">D#</button>
-    <button onclick="playNote('E4')">E</button>
-    <button onclick="playNote('F4')">F</button>
-    <button onclick="playNote('Fs4')">F#</button>
-    <button onclick="playNote('G4')">G</button>
-    <button onclick="playNote('Gs4')">G#</button>
-    <button onclick="playNote('A4')">A</button>
-    <button onclick="playNote('As4')">A#</button>
-    <button onclick="playNote('B4')">B</button>
-    <script>
-        function playNote(note) {
-            fetch(`/playNote/${note}`);
-        }
-|]
+getHomeR = defaultLayout $ do 
+    addStylesheet $ StaticR styles_css
+    [whamlet|
+        <div class="greeting">
+            Hello, welcome to our Piano Music Generator!
+        <div .piano>
+            <button .white-key onclick="playNote('C3')">
+            <button .black-key onclick="playNote('Cs3')">
+            <button .white-key onclick="playNote('D3')">
+            <button .black-key onclick="playNote('Ds3')">
+            <button .white-key onclick="playNote('E3')">
+            <button .white-key onclick="playNote('F3')">
+            <button .black-key onclick="playNote('Fs3')">
+            <button .white-key onclick="playNote('G3')">
+            <button .black-key onclick="playNote('Gs3')">
+            <button .white-key onclick="playNote('A3')">
+            <button .black-key onclick="playNote('As3')">
+            <button .white-key onclick="playNote('B3')">
+            <button .white-key onclick="playNote('C4')">
+            <button .black-key onclick="playNote('Cs4')">
+            <button .white-key onclick="playNote('D4')">
+            <button .black-key onclick="playNote('Ds4')">
+            <button .white-key onclick="playNote('E4')">
+            <button .white-key onclick="playNote('F4')">
+            <button .black-key onclick="playNote('Fs4')">
+            <button .white-key onclick="playNote('G4')">
+            <button .black-key onclick="playNote('Gs4')">
+            <button .white-key onclick="playNote('A4')">
+            <button .black-key onclick="playNote('As4')">
+            <button .white-key onclick="playNote('B4')">
+            <button .white-key onclick="playNote('C5')">
+            <button .black-key onclick="playNote('Cs5')">
+            <button .white-key onclick="playNote('D5')">
+            <button .black-key onclick="playNote('Ds5')">
+            <button .white-key onclick="playNote('E5')">
+            <button .white-key onclick="playNote('F5')">
+            <button .black-key onclick="playNote('Fs5')">
+            <button .white-key onclick="playNote('G5')">
+            <button .black-key onclick="playNote('Gs5')">
+            <button .white-key onclick="playNote('A5')">
+            <button .black-key onclick="playNote('As5')">
+            <button .white-key onclick="playNote('B5')">
+        <script>
+            function playNote(note) {
+                fetch(`/playNote/${note}`);
+            }
+    |]
